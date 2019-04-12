e6bef45c75af81964c2edefff3e9ea0cb4e923cd
Himasriv <himasri.varikuti@gmail.com>
2019-04-12 18:32:43 +0530

    created teammanagement-service

---
+++ teammanagement-service/src/main/java/com/virtusa/sportsManagementSystem/teammanagement_service/TeamManagementApplication.java
@@ -0,0 +1,22 @@
+package com.virtusa.sportsManagementSystem.teammanagement_service;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableEurekaClient
+@EnableFeignClients
+@SpringBootApplication
+public class TeamManagementApplication 
+{
+	private static Logger logger = Logger.getLogger(TeamManagementApplication.class.getClass());
+    public static void main( String[] args )
+    {
+    	PropertyConfigurator.configure("src/main/resource/log4j.properties");
+    	logger.info("Team Management Application is started");
+        SpringApplication.run(TeamManagementApplication.class, args);
+    }
+}
---
+++ teammanagement-service/src/main/java/com/virtusa/sportsManagementSystem/teammanagement_service/service/PlayerService.java
@@ -0,0 +1,43 @@
+package com.virtusa.sportsManagementSystem.teammanagement_service.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.virtusa.sportsManagementSystem.teammanagement_service.domain.Player;
+import com.virtusa.sportsManagementSystem.teammanagement_service.domain.Team;
+import com.virtusa.sportsManagementSystem.teammanagement_service.repository.PlayerRepository;
+import com.virtusa.sportsManagementSystem.teammanagement_service.repository.TeamRepository;
+
+
+@Service
+public class PlayerService {
+	@Autowired
+	TeamRepository teamRepository;
+	@Autowired
+	PlayerRepository playerRepository;
+	
+	public Player registerPlayer(Player player) {
+		Team team = teamRepository.getTeam(player.getTeam().getTeamName());
+		if (team != null) {
+			player.setTeam(team);
+			teamRepository.save(team);
+			return playerRepository.save(player);
+		}
+		 
+		return new Player();
+		
+	}
+	public List<Player> getPlayerListBasedOnRole(String playerRole){
+		List<Player> playerList = playerRepository.getplayersBasedOnRole(playerRole);
+		return playerList;
+	}
+	public List<Player> getPlayerListBasedOnTeam(int id){
+		Team team = teamRepository.findById(id).get();
+		List<Player> playerList = playerRepository.getplayersBasedOnteam(team);
+		playerList.forEach((p)->System.out.println(p));
+		return playerList;
+	}
+
+}
